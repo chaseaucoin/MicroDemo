5795b940f83519982817baffe95da552e4f59528
Chase Q. Aucoin <chase@cupcakefactory.net>
2016-09-05 21:36:23 -0500

    Added roulette signalR demo

---
+++ Final/MicroDemo.Host/Hubs/IotHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroDemo.Host.Hubs
+{
+    public class IotHub : Hub
+    {
+        public void BroadcastSensorStats(int stat)
+        {
+            Clients.All.broadcastStat(stat);
+        }
+    }
+}
--- Final/MicroDemo.Host/Startup.cs
+++ Final/MicroDemo.Host/Startup.cs
@@ -4,6 +4,12 @@ using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.StaticFiles;
 using Microsoft.Owin;
 using Swashbuckle.Application;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using System.Threading.Tasks;
+using MicroDemo.Host.Hubs;
+using System;
+using System.Threading;
 
 namespace MicroDemo.Host
 {
@@ -37,8 +43,23 @@ namespace MicroDemo.Host
                 .EnableSwaggerUi();
 
             appBuilder.UseWebApi(config);
-            
+
+            appBuilder.UseCors(CorsOptions.AllowAll);
+            appBuilder.MapSignalR();
+
             appBuilder.UseFileServer(fileOptions);
+
+            Task.Factory.StartNew(() => {
+                var hub = GlobalHost.ConnectionManager.GetHubContext<IotHub>();
+                var rand = new Random();
+
+                while (true)
+                {
+                    hub.Clients.All.broadcastStat(rand.Next(36));
+
+                    Thread.Sleep(1000);
+                }
+            }, TaskCreationOptions.LongRunning);
         }
     }
 }
